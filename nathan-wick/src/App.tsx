4037791441726cf6fb735f9022b2da63d184b159
Nathan Wick <nathanwick2001@gmail.com>
2022-09-29 20:52:34 -0400

    Fixed Router

--- nathan-wick/src/App.tsx
+++ nathan-wick/src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import About from './components/views/About';
 
 function App() {
   return (
-    <div className='App'>
+    <Router>
       <NavBar />
       <Routes>
         <Route path='/' element={<About />} />
         <Route path='/about' element={<About />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
