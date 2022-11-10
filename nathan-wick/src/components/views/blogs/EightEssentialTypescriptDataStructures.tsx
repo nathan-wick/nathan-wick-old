bd8e76d247cddee8b28a2f005c8f6e7d1846f1fc
Nathan Wick <nathanwick2001@gmail.com>
2022-11-09 19:39:22 -0500

    Update Data Structures Blog

--- nathan-wick/src/components/views/blogs/EightEssentialTypescriptDataStructures.tsx
+++ nathan-wick/src/components/views/blogs/EightEssentialTypescriptDataStructures.tsx
@@ -5,6 +5,7 @@ import Heading from '../../Heading';
 import StackImage from '../../../assets/images/stack.webp'
 import CodeBlock from '../../CodeBlock';
 import QueueImage from '../../../assets/images/queue.webp'
+import ArrayImage from '../../../assets/images/array.webp'
 
 const EightEssentialTypescriptDataStructures = (props: any) => {
     return (
@@ -32,26 +33,21 @@ const EightEssentialTypescriptDataStructures = (props: any) => {
                 text='Stacks are implemented on the Last In First Out (LIFO) basis. A Stack has 2 operations: Push and Pop. Push, adds an element to the top of the collection. Pop, removes an element from the top of the collection.'
                 isMobile={props.isMobile} />
             <CodeBlock
-                text={`/*
-    Stack Data Structure Typescript Code Example
-    By Nathan Wick
-*/
-           
-const rocks = new Stack<string>();
+                text={`const numbers = new Stack<number>();
 
-rocks.push("Basalt");
-rocks.push("Granite");
+numbers.push(1);
+numbers.push(2);
 
-// Output: Basalt, Granite
-console.log(rocks);
+// Output: 1, 2
+console.log(numbers);
 
-const latestRock = rocks.pop();
+const topNumber = numbers.pop();
 
-// Output: Granite
-console.log(latestRock);
+// Output: 2
+console.log(topNumber);
 
-// Output: Basalt
-console.log(rocks);`}
+// Output: 1
+console.log(numbers);`}
                 isMobile={props.isMobile} />
             <Heading
                 text='Queue'
@@ -68,53 +64,52 @@ console.log(rocks);`}
                 text='Queues are implemented on the First In First Out (FIFO) basis. A Queue has 2 operations: Enqueue and Dequeue. Enqueue, adds an element to the back of the collection. Dequeue, removes an element from the front of the collection.'
                 isMobile={props.isMobile} />
             <CodeBlock
-                text={`/*
-    Queue Data Structure Typescript Code Example
-    By Nathan Wick
-*/
+                text={`const numbers = new Queue<number>();
 
-const peopleInLine = new Queue<string>();
+numbers.enqueue(1);
+numbers.enqueue(2);
 
-peopleInLine.enqueue("Nate Dogg");
-peopleInLine.enqueue("Big John");
+// Output: 1, 2
+console.log(numbers);
 
-// Output: Nate Dogg, Big John
-console.log(peopleInLine);
+const frontNumber = numbers.dequeue();
 
-const nextPerson = peopleInLine.dequeue();
+// Output: 1
+console.log(frontNumber);
 
-// Output: Nate Dogg
-console.log(nextPerson);
-
-// Output: Big John
-console.log(peopleInLine);`}
+// Output: 2
+console.log(numbers);`}
                 isMobile={props.isMobile} />
             <Heading
                 text='Array'
                 isMobile={props.isMobile} />
+            <div
+                className='text-center'>
+                <img
+                    className={`${props.isMobile ? `w-100` : `w-50`} rounded p-3`}
+                    src={ArrayImage}
+                    alt='Array Data Structure by Nathan Wick'>
+                </img>
+            </div>
             <Text
-                text='Arrays store data of the same type. Data can be accessed anywhere within an array.'
+                text='Arrays store data of a defined type with a fixed size. With an Array, data can be accessed anywhere.'
                 isMobile={props.isMobile} />
             <CodeBlock
-                text={`/*
-    Array Data Structure Typescript Code Example
-    By Nathan Wick
-*/
+                text={`
+const numbers: number[] = [ 1, 2, 3 ];
 
-const colors: string[] = [ 'Red', 'Blue', 'Yellow' ];
+// Output: 1, 2, 3
+console.log(numbers);
 
+// Output: 1
+console.log(numbers[0]);
 // Output: 3
-console.log(colors.length);
+console.log(numbers[2]);
 
-// Output: Red
-console.log(colors[0]);
-// Output: Yellow
-console.log(colors[2]);
+numbers[1] = 4;
 
-colors[1] = 'Green';
-
-// Output: Red, Green, Yellow
-console.log(colors)`}
+// Output: 1, 4, 3
+console.log(numbers)`}
                 isMobile={props.isMobile} />
             <Heading
                 text='Hash Table'
